6c78dd0f23db5314b491e101c9dcb0539c7b5609
VenkatSaaii <saiboda520@gmail.com>
2024-03-25 22:05:51 +0530

    changes in the expenses preformence

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React,{useState}from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense";
+const Dummy_expenses= [
+  { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+  { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+  { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+  { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+];
 
 function App() {
-  const expenses = [
-    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
-    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
-    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
-    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
-  ];
+  const [expenses, setExpenses]=useState(Dummy_expenses)
+ 
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense)
+    
+    setExpenses(prevExpenses=>{
+      return [expense, ...prevExpenses]
+
+      
+    })
   }
 
   return (
--- src/components/ExpenseFilter.js
+++ src/components/ExpenseFilter.js
@@ -1,19 +1,30 @@
 import React from "react";
-const ExpenseFilter=(props)=>{
-    const dropdownhandler=(event)=>{
-    props.onChangeHandler(event.target.value);
-} 
-return(
-    <div className="expenses-filter">
-        <div className="expenses-filter__control"></div>
-        <label >Filter By Year</label>
-        <select value={props.selected} onChange={dropdownhandler}></select>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="2020">202</option>
-        <option value="2019">2019</option>
 
+const ExpenseFilter = (props) => {
+    const dropdownHandler = (event) => {
+      try {
+        // Update the selected year state by calling the onChange function passed as props
+        props.onChange(event.target.value);
+      } catch (error) {
+        console.error("Error in dropdownHandler:", error);
+      }
+    };
+  
+  
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter By Year</label>
+        <select value={props.selected}onChange={dropdownHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
     </div>
-)
-}
-export default ExpenseFilter
+  );
+};
+
+
+export default ExpenseFilter;
